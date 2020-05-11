c8c68e749445a1992b80ba80411be07fcffcdcd1
Hannes Samskog <hannessamskog@gmail.com>
2020-05-11 21:30:43 +0200

    favicon

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Orbitron&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <link rel="stylesheet" href="assets/css/styles.css">
-        
+        <link rel="shortcut icon" type="image/jpg" href="assets/img/kdLogga.png"/>
     </head>
 
     <?php
