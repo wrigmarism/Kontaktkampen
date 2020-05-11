09e4fceac190b7cc41a7bd4fbda3e679eef122ac
Christoffer Wrigmar <christoffer.wrigmar@gmail.com>
2020-05-11 16:43:36 +0200

    js ändringar

--- index.php
+++ index.php
@@ -29,20 +29,12 @@
         messagingSenderId: "927975566314",
         appId: "1:927975566314:web:6ae89ddc756572c8496093",
         measurementId: "G-D3S89K248G"
-        
+
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
   </script>
 
-        <!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src="/__/firebase/7.14.3/firebase-app.js"></script>
-
-<!-- TODO: Add SDKs for Firebase products that you want to use
-     https://firebase.google.com/docs/web/setup#available-libraries -->
-<script src="/__/firebase/7.14.3/firebase-analytics.js"></script>
-
-<!-- Initialize Firebase -->
-<script src="/__/firebase/init.js"></script>
+    <script src="assets/js/functions.js"></script>
     </body>
 </html>
