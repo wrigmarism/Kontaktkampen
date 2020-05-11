6ffab96bb20c704c8e6236b5114caa896fedc31d
Hannes Samskog <hannessamskog@gmail.com>
2020-05-11 20:06:18 +0200

    menyn fungerar nu

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -2,102 +2,127 @@
 firebase.initializeApp({
   apiKey: "AIzaSyB79OZRXPSHds112xsBihTbG-Iv0kjKPt8",
   authDomain: "kontaktkampen-b308a.firebaseapp.com",
-  projectId: "kontaktkampen-b308a"
+  projectId: "kontaktkampen-b308a",
 });
 
 var db = firebase.firestore();
 
-function home(){
-    var xhttp;
-    if (window.XMLHttpRequest) {
-      xhttp = new XMLHttpRequest();
-    } else {
-      xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+$(document).ready(function () {
+  $("#home_button").click(function () {
+    home();
+  });
+});
+
+$(document).ready(function () {
+  $("#companies_button").click(function () {
+    companies();
+  });
+});
+
+$(document).ready(function () {
+  $("#logIn_button").click(function () {
+    logIn();
+  });
+});
+
+$(document).ready(function () {
+  $("#logOut_button").click(function () {
+    logOut();
+  });
+});
+
+function home() {
+  var xhttp;
+  if (window.XMLHttpRequest) {
+    xhttp = new XMLHttpRequest();
+  } else {
+    xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("content").innerHTML = this.responseText;
     }
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("content").innerHTML = this.responseText;
-      }
-    };
-    xhttp.open("GET", "include/html/home.php", true);
-    xhttp.send();
-};
-  
-  
-function companies(){
-  document.getElementById("content").innerHTML ='<h1>Årets företag</h1>' +
-  '<table id="company" cellspacing="0" cellpadding="1">' +
-      '<tr>' +
-          '<th> Logga </th>' +
-          '<th> Företag </th>' +
-          '<th> Kod </th>' +
-      '</tr>' +
-      '<tr>';
+  };
+  xhttp.open("GET", "include/html/home.php", true);
+  xhttp.send();
+}
 
-  db.collection("company").get().then(function(querySnapshot) {
-    querySnapshot.forEach(function(doc) {
-      $("#content").append('<tr><th><img src="' + doc.data().img + '" width="50"></th>' +
-                            '<th>' + doc.data().name + '</th>' +
-                            '<th>' + doc.data().code + '</th></tr>');
-  })});
-  
-  
+function companies() {
+  document.getElementById("content").innerHTML =
+    "<h1>Årets företag</h1>" +
+    '<table id="company" cellspacing="0" cellpadding="1">' +
+    "<tr>" +
+    "<th> Logga </th>" +
+    "<th> Företag </th>" +
+    "<th> Kod </th>" +
+    "</tr>" +
+    "<tr>";
 
+  db.collection("company")
+    .get()
+    .then(function (querySnapshot) {
+      querySnapshot.forEach(function (doc) {
+        $("#content").append(
+          '<tr><th><img src="' +
+            doc.data().img +
+            '" width="50"></th>' +
+            "<th>" +
+            doc.data().name +
+            "</th>" +
+            "<th>" +
+            doc.data().code +
+            "</th></tr>"
+        );
+      });
+    });
+}
 
-};
+// var xhttp;
+// if (window.XMLHttpRequest) {
+//   xhttp = new XMLHttpRequest();
+// } else {
+//   xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+// }
+// xhttp.onreadystatechange = function() {
+//   if (this.readyState == 4 && this.status == 200) {
+//     document.getElementById("content").innerHTML = this.responseText;
+//   }
+// };
+// xhttp.open("GET", "include/html/companies.php", true);
+// xhttp.send();
 
-  
-
-
-    // var xhttp;
-    // if (window.XMLHttpRequest) {
-    //   xhttp = new XMLHttpRequest();
-    // } else {
-    //   xhttp = new ActiveXObject("Microsoft.XMLHTTP");
-    // }
-    // xhttp.onreadystatechange = function() {
-    //   if (this.readyState == 4 && this.status == 200) {
-    //     document.getElementById("content").innerHTML = this.responseText;
-    //   }
-    // };
-    // xhttp.open("GET", "include/html/companies.php", true);
-    // xhttp.send();
-
-
-function logIn(){
-    var xhttp;
-    if (window.XMLHttpRequest) {
-      xhttp = new XMLHttpRequest();
-    } else {
-      xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+function logIn() {
+  var xhttp;
+  if (window.XMLHttpRequest) {
+    xhttp = new XMLHttpRequest();
+  } else {
+    xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("content").innerHTML = this.responseText;
     }
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("content").innerHTML = this.responseText;
-      }
-    };
-    xhttp.open("POST", "include/html/login.php", true);
-    xhttp.send();
+  };
+  xhttp.open("POST", "include/html/login.php", true);
+  xhttp.send();
+}
 
-};
-
-function logOut(){
+function logOut() {
   window.location = "include/models/logout.php";
-};
+}
 
-function register(){
-    var xhttp;
-    if (window.XMLHttpRequest) {
-      xhttp = new XMLHttpRequest();
-    } else {
-      xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+function register() {
+  var xhttp;
+  if (window.XMLHttpRequest) {
+    xhttp = new XMLHttpRequest();
+  } else {
+    xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("content").innerHTML = this.responseText;
     }
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("content").innerHTML = this.responseText;
-      }
-    };
-    xhttp.open("POST", "include/html/registerform.php", true);
-    xhttp.send();
-};
-
+  };
+  xhttp.open("POST", "include/html/registerform.php", true);
+  xhttp.send();
+}
--- include/menu.php
+++ include/menu.php
@@ -1,17 +1,3 @@
-<!-- <div class="container" id="menu"> -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-blue">
-    <button class="btn btn-secondary" type="button" onclick="home()">Start</button>
-    <button class="btn btn-secondary" type="button" onclick="companies()">Företag</button>
-    <?php if(isset($_SESSION['status']) && $_SESSION['status'] == true) : ?>
-        <button class="btn btn-secondary" type="button"><?php echo $_SESSION["uid"] ?></button>
-        <button class="btn btn-secondary" type="button" onclick="logOut()">Logga ut</button>
-    <?php else : ?>
-                <button class="btn btn-secondary" type="button" onclick="logIn()">Logga in</button>
-                <button class="btn btn-secondary" type="button" onclick="register()">Registera dig</button>
-    <?php endif ?> 
-    </nav>
-<!-- </div> -->
-
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
 <a class="navbar-brand" href="#">
     <img src="/assets/img/kdLogga.png" width="30" height="30" class="d-inline-block align-top" alt="">
@@ -22,9 +8,15 @@
   </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
-      <a class="nav-item nav-link "href="#">Home </a>
-      <a class="nav-item nav-link" href="#">Företag</a>
-      <a class="nav-item nav-link" href="#">Logga in</a>
+      <a class="nav-item nav-link" id="home_button">Home </a>
+      <a class="nav-item nav-link" id="companies_button">Företag</a> 
+      
+      <?php if(isset($_SESSION['status']) && $_SESSION['status'] == true) : ?>
+      <a class="nav-item nav-link" id="logIn_button">Logga in</a>
+    <?php else : ?>
+      <a class="nav-item nav-link" id="logOut_button">Logga ut</a>
+    <?php endif ?> 
+
     </div>
   </div>
 </nav>
