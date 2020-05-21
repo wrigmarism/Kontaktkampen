3ecc889ac93685e64128581c77b5c261eeae5d00
HannesSam <hannessamskog@gmail.com>
2020-05-21 18:35:53 +0200

    kod till Simon

--- src/components/companyContainer.jsx
+++ src/components/companyContainer.jsx
@@ -1,46 +1,52 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { getData } from '../helpers/db';
+import { getData } from "../helpers/db";
+import Company from "../helpers/companyClass.js";
+import CompanyCard from "./companyCard";
 
-
-import Accordion from 'react-bootstrap/Accordion';
-
+import Accordion from "react-bootstrap/Accordion";
+/* Denna komponents syfte är att ta in datan från databasen och sedan för varje objekt i databasen skapa 
+en ny komponent av typen "companyCard" med medföljande data. */
 class CompanyContainer extends React.Component {
-    constructor(props) {
-      super(props);
+  constructor(props) {
+    super(props);
 
-      this.state = {
-        hits: [],
-      };
-    }
-
-    async componentDidMount() {
-      var data = await getData("company");
-      var list = [];
-        // data.then(function(result) {
-        //     result.forEach(function (doc) {
-        //         list.push(doc);
-        //         });
-                
-            // console.log(list[1]);
-            // });
-            
-            this.setState({ hits: data });
-    }
-  
-    render() {
-      const { hits } = this.state;
-   
-      return (
-        <Accordion> 
-          {hits.map(hit =>
-            <li key={hit.objectID}>
-              <a href={hit.name}>{hit.name}</a>
-            </li>
-          )}
-        </Accordion> 
-      );
-    }
+    this.state = {
+      companies: [],
+    };
   }
 
-  export default CompanyContainer;
+  async componentDidMount() {
+    var data = getData("company");
+    var list = [];
+    data.then(function(result) {
+        result.forEach(function (doc) {
+            list.push(doc);
+            });
+    });
+    console.log(list);
+    /* Lyckas inte få över listan till min comapnies array 
+     När jag lyckades med det i en annan variant så skrev den ut allt två gånger alternativt uppdaterade inte när 
+     datan hade laddat in. Så tror bara jag tänker lite knasigt på hur man gör detta på smidigast sätt. Help me obiwan Simpelimp your my only hope*/
+    this.setState({companies: list});
+  }
+
+
+  render() {
+    const companies = this.state.companies.map((name, move) => {
+      return (
+        <li key={move}>
+          {name}
+        </li>
+      );
+    });
+    return (
+      <Accordion>
+        <CompanyCard />
+        {companies}
+      </Accordion>
+    );
+  }
+}
+
+export default CompanyContainer;
