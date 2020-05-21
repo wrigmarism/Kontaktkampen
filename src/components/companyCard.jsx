4682e3115a20173378606ab6ab35f1cdda347bf1
HannesSam <hannessamskog@gmail.com>
2020-05-21 18:32:24 +0200

    kod som inte funkar

--- src/components/companyCard.jsx
+++ src/components/companyCard.jsx
@@ -1,29 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { getData } from '../helpers/db';
+import React from "react";
+import ReactDOM from "react-dom";
+import { getData } from "../helpers/db";
 
-import Accordion from 'react-bootstrap/Accordion';
+import Accordion from "react-bootstrap/Accordion";
 
 class CompanyCard extends React.Component {
-    constructor(props) {
-      super(props);
-      console.log(props);
-      this.state = {
-        hits: [],
-      };
-    }
-
-    async componentDidMount() {
-      
-    }
-  
-    render() {
-      const { hits } = this.state;
-   
-      return (
-        <h1>"jag är ett företag"</h1>
-      );
-    }
+  constructor(props) {
+    super(props);
+    console.log(props);
+    this.state = {
+      hits: [],
+    };
   }
 
-  export default CompanyCard;
+  async componentDidMount() {}
+
+  render() {
+    const { hits } = this.state;
+
+    return <h1>"jag är ett företag"</h1>;
+  }
+}
+
+export default CompanyCard;
--- src/helpers/db.js
+++ src/helpers/db.js
@@ -1,12 +1,21 @@
 import { db } from "../services/firebase";
-import Company from './companyClass.js'
+import Company from "./companyClass.js";
 
 export async function getData(collection) {
   const data = await db.collection(collection).get();
   let result = [];
   data.forEach(function (doc) {
-    var company = new Company(doc.id, doc.get("name"), doc.get("title"), doc.get("text"), doc.get("question"), 
-    doc.get("answer1"), doc.get("answer2"), doc.get("answer3"), doc.get("correctAnswer"));
+    var company = new Company(
+      doc.id,
+      doc.get("name"),
+      doc.get("title"),
+      doc.get("text"),
+      doc.get("question"),
+      doc.get("answer1"),
+      doc.get("answer2"),
+      doc.get("answer3"),
+      doc.get("correctAnswer")
+    );
     result.push(company);
   });
   return result;
