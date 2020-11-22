5fb546c0949f126d27842ba2251b588d8ce1b627
Christoffer Wrigmar <50480487+wrigmarism@users.noreply.github.com>
2020-11-22 21:23:18 +0100

    v1.0.0. MADDAFAKKAS

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -23,7 +23,6 @@ class Login extends Component {
   }
 
   async handleSubmit(e) {
-    console.log(this.state.email);
     e.preventDefault();
     auth.setPersistence(fireB.auth.Auth.Persistence.LOCAL).then((u) => {
       return auth
